329cb5720e6eb8e8eac6e8e32ea0ba6b4e4b2532
ejensen <jensen39@gmail.com>
2011-04-06 16:00:08 +0000

    Fixed a potential error in getting the user's username.

--- Extension/scripts/inject.js
+++ Extension/scripts/inject.js
@@ -34,11 +34,13 @@ var Inject = {
 	},
 	
     getUsername: function () {
-        var usernameElem = document.createElement("span");
-        usernameElem.id = "username";
-        usernameElem.style.display = "none";
-        usernameElem.innerText = window.chat.username;
-        document.getElementById("chat-wrapper").appendChild(usernameElem);
+		if(window.chat && window.chat.username){
+			var usernameElem = document.createElement("span");
+			usernameElem.id = "username";
+			usernameElem.style.display = "none";
+			usernameElem.innerText = window.chat.username;
+			document.getElementById("chat-wrapper").appendChild(usernameElem);
+		}
     }
 };
 
