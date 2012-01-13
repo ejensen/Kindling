eb66b33bfeee98103c6109b8aa2e408a7a80edb2
Evan Dontje <e.dontje@techsmith.com>
2012-01-13 14:38:51 -0500

    Defaulting filtering to wide open

--- src/scripts/kindling.background.js
+++ src/scripts/kindling.background.js
@@ -66,8 +66,8 @@ kindling.module(function () {
          initSetting('timeStamps', 'true');
          initSetting('showAvatars', 'true');
          initSetting('filterNotifications', 'false');
-         initSetting('filterNotificationsByCustom', 'true');
-         initSetting('customFilter', 'HI');
+         initSetting('filterNotificationsByCustom', 'false');
+         initSetting('customFilter', '.*');
          initSetting('autoDismiss', 'true');
          initSetting('notifications', 'true');
          initSetting('notificationTimeout', '5000');
