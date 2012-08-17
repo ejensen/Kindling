e75629178534d68bac4efe421bc182cd71a068b6
Darrin Holst <darrinholst@gmail.com>
2012-08-17 08:04:38 -0500

    First crack at hiding extra timestamp messages.

--- src/scripts/kindling.messagefilter.js
+++ src/scripts/kindling.messagefilter.js
@@ -1,28 +1,75 @@
 kindling.module(function () {
-	'use strict';
+  'use strict';
 
-	function showHideElements(key, value) {
-		var $chat = $('#chat-wrapper');
-		if (value === 'false' && $chat.hasClass(key) === false) {
-			$chat.addClass(key);
-			kindling.scrollToBottom();
-		} else if (value === 'true' && $chat.hasClass(key) === true) {
-			$chat.removeClass(key);
-			kindling.scrollToBottom();
-		}
-	}
+  var $lastTimestampMessage = null;
 
-	function filterMessages(e, options) {
-		if (options) {
-			showHideElements('noEnterRoom', options.enterRoom);
-			showHideElements('noLeaveRoom', options.leaveRoom);
-			showHideElements('noTimeStamp', options.timeStamps);
-		}
-	}
+  function isLeaveMessage($message) {
+    return $message.hasClass('leave_message') || $message.hasClass('kick_message');
+  }
 
-	return {
-		init: function () {
-			$.subscribe('optionsChanged', filterMessages);
-		}
-	};
+  function isEnterMessage($message) {
+    return $message.hasClass('enter_message')
+  }
+
+  function isTimestampMessage($message) {
+    return $message.hasClass('timestamp_message')
+  }
+
+  function showMessage($message) {
+    if($message) {
+      $message.removeClass('hidden_message');
+    }
+  }
+
+  function hideMessage($message) {
+    if($message) {
+      $message.addClass('hidden_message');
+    }
+  }
+
+  function shouldHideThisMessage($message, options) {
+    return (options.leaveRoom === 'false' && isLeaveMessage($message)) ||
+           (options.enterRoom === 'false' && isEnterMessage($message)) ||
+           (options.timeStamps === 'false' && isTimestampMessage($message));
+  }
+
+  function filterMessage(e, options, username, message) {
+    var $message = $(message);
+
+    if($message.hasClass("message")) {
+      var hideCurrentMessage = shouldHideThisMessage($message, options);
+
+      if(isTimestampMessage($message)) {
+        hideMessage($lastTimestampMessage);
+        $lastTimestampMessage = $message;
+      } else if(!hideCurrentMessage) {
+        $lastTimestampMessage = null;
+      }
+
+      if(hideCurrentMessage) {
+        hideMessage($message);
+      } else {
+        showMessage($message);
+      }
+    }
+  }
+
+  function filterMessages(e, options) {
+    if (options) {
+      $('#chat-wrapper .message').each(function(index, message) {
+        filterMessage(e, options, null, message);
+      });
+
+      hideMessage($lastTimestampMessage);
+      kindling.scrollToBottom();
+    }
+  }
+
+  return {
+    init: function () {
+      $.subscribe('optionsChanged', filterMessages);
+      $.subscribe('newMessage', filterMessage);
+    }
+  };
 }());
+
